7c169501e66b84f8d455129c75e14dd4691b0d4b
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2023-11-17 22:43:16 +0900

    feat (client) : Router & RTK - Profile Route
    - Add a new route to `Profile` page
    - Create the base of the `Profile` page
    - Set route protections based on the authentication

---
+++ client/src/pages/ProfilePage/index.js
@@ -0,0 +1,9 @@
+// Layout
+import MainLayout from "../layouts/MainLayout";
+
+// Page
+function ProfilePage() {
+  return <MainLayout>Test</MainLayout>;
+}
+
+export default ProfilePage;
--- client/src/router/App.js
+++ client/src/router/App.js
@@ -1,21 +1,37 @@
 // Imports
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Routes
 import routesList from "./routesList";
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+import { setToken } from "../redux/authSlice";
 
 // Components
 import HomePage from "../pages/HomePage/index";
 import LoginPage from "../pages/LoginPage/index";
 import Error404Page from "../pages/404/Error404Page";
+import Profile from "../pages/ProfilePage";
 
 // Router
 function App() {
+  const dispatch = useDispatch();
+
+  // Set token from the persistant one if the page is reloaded
+  const isAuth = useSelector((state) => state.auth.isValid);
+  const token = localStorage.getItem("token");
+  if (!isAuth && token !== null && token.length > 0) {
+    dispatch(setToken(token));
+  }
+
   return (
     <Router>
       <Routes>
         <Route path={routesList.home} element={<HomePage />} />
-        <Route path={routesList.login} element={<LoginPage />} />
+
+        {isAuth ? (
+          <Route path={routesList.profile} element={<Profile />} />
+        ) : (
+          <Route path={routesList.login} element={<LoginPage />} />
+        )}
 
         {/* Fallback page if nothing matches */}
         <Route path={routesList.routeCatcher} element={<Error404Page />} />
