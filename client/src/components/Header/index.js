fad7dfc3b8d43c74f620a9cdd788c7c69f1d0e82
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2023-11-18 16:18:05 +0900

    feat & style (client) : Header & RTK - Authenticated User Interface
    - Use the Redux state as condition to display either log-in or log-out buttons
    - Add a function to reset token in localStorage & Redux store and redirect the user to the Home page
    - Add new styling rules to better display the 2 new buttons in the `Header` replacing the log-in button

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,10 @@
 // Imports
 import { NavLink, useLocation } from "react-router-dom";
 
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../redux/authSlice";
+
 // Routing
 import routesList from "../../router/routesList";
 
@@ -10,7 +14,17 @@ import Logo from "../../assets/img/argentBankLogo.png";
 
 // Component
 function Navbar() {
+  // Authentication
+  const isAuth = useSelector((state) => state.auth.isValid);
+  const dispatch = useDispatch();
+
   const currentLocation = useLocation();
+
+  const user = useSelector((state) => state.user);
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
 
   return (
     <header className={styles["main-nav"]}>
@@ -30,17 +44,42 @@ function Navbar() {
       </NavLink>
 
       <nav>
-        <NavLink
-          to={routesList.login}
-          className={({ isActive }) =>
-            isActive
-              ? styles["router-link-exact-active"]
-              : styles["main-nav-item"]
-          }
-        >
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </NavLink>
+        {isAuth ? (
+          <div className={styles["main-nav-items"]}>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? styles["router-link-exact-active"]
+                  : styles["main-nav-item"]
+              }
+              to={routesList.profile}
+            >
+              <i className="fa fa-user-circle"></i>
+              {user.firstname}
+            </NavLink>
+
+            <NavLink
+              to={routesList.home}
+              onClick={logoutHandler}
+              className={styles["main-nav-item"]}
+            >
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </NavLink>
+          </div>
+        ) : (
+          <NavLink
+            to={routesList.login}
+            className={({ isActive }) =>
+              isActive
+                ? styles["router-link-exact-active"]
+                : styles["main-nav-item"]
+            }
+          >
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </NavLink>
+        )}
       </nav>
     </header>
   );
