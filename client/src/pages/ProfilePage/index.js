e9af9bbb425ba773cd28b6c59f3b981426fcc1f5
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2023-11-18 21:28:16 +0900

    feat & style (client) : Profile Page Update
    - Create the page which contains the user profile
    - Use one of the `userSlice` properties to decide which component to display (either the `<Welcome />` or `<NameEditMode />`)
    - Create a specific stylesheet for this component
    
    - Delete `user.html` as it was replaced by `<Profile />` component
    - Delete `main.css` file as all of this content was dispatched all over the project

--- client/src/pages/ProfilePage/index.js
+++ client/src/pages/ProfilePage/index.js
@@ -1,9 +1,28 @@
+// Redux
+import { useSelector } from "react-redux";
+
+// Components
+import Welcome from "../../components/Welcome/";
+import NameEditMode from "../../components/Welcome/NameEditMode";
+import Accounts from "../../components/Accounts/index";
+
 // Layout
 import MainLayout from "../layouts/MainLayout";
 
+// Styles
+import styles from "./ProfilePage.module.css";
+
 // Page
 function ProfilePage() {
-  return <MainLayout>Test</MainLayout>;
+  const isEditMode = useSelector((state) => state.user.isEditMode);
+
+  return (
+    <MainLayout customStyle={styles["bg-dark"]}>
+      {!isEditMode ? <Welcome /> : <NameEditMode />}
+
+      <Accounts />
+    </MainLayout>
+  );
 }
 
 export default ProfilePage;
