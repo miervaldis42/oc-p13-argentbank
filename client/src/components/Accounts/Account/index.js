c80d124ef166b3c1e4afe2e691ddefb7acd88824
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2023-11-18 21:14:06 +0900

    feat & style (client) : Account Component Creation
    - Create a component to display each user account
    - Create a specific stylesheet for the component
    - Use `props` to receive dynamic data

---
+++ client/src/components/Accounts/Account/index.js
@@ -0,0 +1,23 @@
+// Styles
+import styles from "./Account.module.css";
+
+// Component
+function Account({ title, amount, description }) {
+  return (
+    <section className={styles.account}>
+      <div className={styles["account-content-wrapper"]}>
+        <h3 className={styles["account-title"]}>{title}</h3>
+        <p className={styles["account-amount"]}>{amount}</p>
+        <p className={styles["account-amount-description"]}>{description}</p>
+      </div>
+
+      <div className={styles["account-content-wrapper cta"]}>
+        <button className={styles["transaction-button"]}>
+          View transactions
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default Account;
